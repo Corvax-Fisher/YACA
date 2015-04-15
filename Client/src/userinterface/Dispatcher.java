0759b8f3a4aef00f063598493913f3a42926200a
farahzeb <farahzeb@hotmail.de>
2015-04-15 10:45:07 +0200

    implemented first version of front controller

---
+++ Client/src/userinterface/Dispatcher.java
@@ -0,0 +1,44 @@
+package userinterface;
+
+public class Dispatcher {
+	private MainView mainView;
+	private LoginView loginView;
+	private RegisterView registerView;
+	private ProfileView profileView;
+	private RoomListView roomListView;
+	private UserListView userListView;
+	private ChatView chatView;
+
+	public Dispatcher(){
+		mainView = new MainView();
+		loginView = new LoginView();
+		registerView = new RegisterView();
+		profileView = new ProfileView();
+		roomListView = new RoomListView();
+		userListView = new UserListView();
+		chatView = new ChatView();
+	}
+
+	public void dispatch(String request){
+		mainView.setVisible(true);
+		
+		if(request.equalsIgnoreCase("LOGIN")){
+			mainView.add(loginView);
+		}
+		else if(request.equalsIgnoreCase("REGISTER")){
+			mainView.add(registerView);
+		}
+		else if(request.equalsIgnoreCase("PROFILE")){
+			mainView.add(profileView);
+		}
+		else if(request.equalsIgnoreCase("ROOMLIST")){
+			mainView.add(roomListView);
+		}
+		else if(request.equalsIgnoreCase("USERLIST")){
+			mainView.add(userListView);
+		}
+		else if(request.equalsIgnoreCase("CHAT")){
+			mainView.add(chatView);
+		}
+	}
+}
---
+++ Client/src/userinterface/FrontControllerDemo.java
@@ -0,0 +1,11 @@
+package userinterface;
+
+
+public class FrontControllerDemo {
+	public static void main(String[] args) {
+
+		FrontController frontController = new FrontController();
+		frontController.dispatchRequest("REGISTER");
+		frontController.dispatchRequest("LOGIN");
+	}
+}
